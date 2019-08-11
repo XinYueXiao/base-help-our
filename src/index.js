3f9677a804affa32c58d2514071a0b5a8b8d3213
王新月 <31272499+XinYueXiao@users.noreply.github.com>
2019-08-11 13:02:29 +0800

    调整demo布局

--- src/index.js
+++ src/index.js
@@ -8,15 +8,17 @@ class App extends React.Component {
   render() {
     return (
       <div className="demo">
-        <article>
-          <h1>树形穿梭框 </h1>
-          <em>核心：数据的流转，数据选中/取消同步</em>
+        <section>
+          <article>
+            <h1>树形穿梭框 </h1>
+            <em>核心：数据的流转，数据选中/取消同步</em>
+          </article>
           <TreeTransfer
             options={dataJson.hotCity}
             onClickCancel={() => console.log("隐藏")}
             onOkClick={list => console.log("保存的筛选项", list)}
           />
-        </article>
+        </section>
       </div>
     );
   }
